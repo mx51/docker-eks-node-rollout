bc14ebca04e901be91dd8b463f53e09289971aa7
Aaron Gorka <aaron.gorka@cmdsolutions.com.au>
2019-11-20 13:23:55 +1100

    Tweak timings

--- eks_node_rollout/eks_node_rollout.py
+++ eks_node_rollout/eks_node_rollout.py
@@ -203,11 +203,12 @@ def rollout_nodes(cluster_name, dry_run, debug):
             before_instance_count = get_num_of_instances(asg_client=asg_client, ec2_client=ec2_client, asg_name=asg_name)
             add_time = datetime.datetime.now(datetime.timezone.utc)
             add_node(asg_client=asg_client, asg_name=asg_name, dry_run=dry_run)
-            time.sleep(5)  # new instance takes a bit to show up in API, don't bother polling yet
+            logging.info(f'Waiting for instance {latest_node_name} to be created...')
+            time.sleep(25)  # new instance takes a bit to show up in API, don't bother polling yet
             latest_instance = get_latest_instance(asg_client=asg_client, ec2_client=ec2_client, asg_name=asg_name, add_time=add_time, dry_run=dry_run)
             latest_node_name = latest_instance["PrivateDnsName"]
             logging.info(f'Waiting for instance {latest_node_name} to be "Ready"...')
-            time.sleep(45)  # instance will never be ready before this, don't bother polling yet
+            time.sleep(25)  # instance will never be ready before this, don't bother polling yet
             wait_for_ready_node(latest_node_name)
             logging.info(f'Node {latest_node_name} is now "Ready".')
             after_instance_count = get_num_of_instances(asg_client=asg_client, ec2_client=ec2_client, asg_name=asg_name)
